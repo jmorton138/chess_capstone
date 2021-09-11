bfd32ccf492bc16484f7c011465f5bf66b31664e
jmorton138 <jmorton138@gmail.com>
2021-09-11 09:16:48 +0100

    build_grid method in Gameboard class

---
+++ lib/main.rb
@@ -0,0 +1,21 @@
+class Gameboard
+    def initialize
+        @grid = build_grid()
+    end
+
+    def build_grid
+        letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+        grid = []
+        for letter in letters
+            i = 1
+            og_letter = letter
+            until i > 8 do
+                square = "#{og_letter}#{i.to_s}"
+                grid.push(square)
+                i += 1
+            end
+        end
+        grid
+    end
+end
+
