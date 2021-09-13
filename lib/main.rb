2288a56fcf3087da690340168c191fbd15d063d5
jmorton138 <jmorton138@gmail.com>
2021-09-13 10:47:05 +0100

    find_rook_moves method returns y-axis available moves when no obstructions

--- lib/main.rb
+++ lib/main.rb
@@ -61,6 +61,24 @@ class Gameboard
         #return space coordinates
     end
 
+    def find_rook_moves(start_pt)
+        moves = []
+        split_point = start_pt.split(//)
+        #x-axis moves
+        #ascii_val = split_point[0].ord
+        #y-axis moves
+        i = 1
+        until i == 8
+            temp = split_point
+            temp[1] = temp[1].to_i + 1
+            moves.push(temp.join())
+            i += 1 
+        end
+        moves
+       
+
+    end
+
 end
 
 
@@ -69,3 +87,18 @@ end
 # board = Gameboard.new
 # board.display_grid(board.grid)
 
+# a8  b8  c8  d8  e8  f8  g8  h8 
+# --------------------------------
+#  a7  b7  c7  d7  e7  f7  g7  h7 
+# --------------------------------
+#  a6  b6  c6  d6  e6  f6  g6  h6 
+# --------------------------------
+#  a5  b5  c5  d5  e5  f5  g5  h5 
+# --------------------------------
+#  a4  b4  c4  d4  e4  f4  g4  h4 
+# --------------------------------
+#  a3  b3  c3  d3  e3  f3  g3  h3 
+# --------------------------------
+#  a2  b2  c2  d2  e2  f2  g2  h2 
+# --------------------------------
+#  a1  b1  c1  d1  e1  f1  g1  h1 
