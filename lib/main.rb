5908adc4089df39be913049dd3dece1251f072c5
jmorton138 <jmorton138@gmail.com>
2021-09-14 12:12:40 +0100

    #update_player_moves updates value in Player.moves hash

--- lib/main.rb
+++ lib/main.rb
@@ -304,22 +304,20 @@ class Player
         end
         pieces
     end
+
+    def update_player_moves(start_pt, end_pt)
+        key = nil
+        moves.each do |item|
+            key = item[0] if item[1] == start_pt
+        end 
+        moves[key] = end_pt
+    end
+
 end
 
 
 
-# board = Gameboard.new
-# p1 = Player.new("white")
-# # p board.validate_player_input("b3b4", p1.moves)
 
-# p2 = Player.new("black")
-# p board.return_piece_type("e1",p1.moves)
-# p2.moves.each do |item|
-#     item if item[1] == "a7"
-# end
-# p p2.moves.key("a7")
-
-#board.display_grid(board.grid, p1.moves, p2.moves)
 
 # a8  b8  c8  d8  e8  f8  g8  h8 
 # --------------------------------
