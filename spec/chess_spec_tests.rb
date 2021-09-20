e8f94c720b70e549fd15800f377615cf42aaf898
jmorton138 <jmorton138@gmail.com>
2021-09-20 18:01:59 +0100

    Add test for Rook.find_moves for multiple obstructions by own pieces. Test passes

--- spec/chess_spec_tests.rb
+++ spec/chess_spec_tests.rb
@@ -195,19 +195,18 @@ describe Rook do
             end
         end
 
-#         context "when white rook is at c4 with player's own peices obstructing at c5 and at f4" do
-#             subject(:rook_blocked_twice) { described_class.new }
+        context "when white rook is at c4 with player's own peices obstructing at c5 and at f4" do
+            subject(:rook_blocked_twice) { described_class.new("c4", "white") }
 
-#             it "returns ['a4', 'b4','d4', 'e4', 'c1', 'c2', 'c3']" do
-#                 player_moves[:bish1] = "f4"
-#                 player_moves[:pawn3] = "d5"
-#                 player_moves[:bish2] = "c5"
-#                 opp_moves = {}
-#                 moves = ["a4", "b4","d4", "e4", "c1", "c2", "c3"]
-#                 expect(rook_blocked_twice.find_rook_moves("c4", player_moves, opp_moves)).to eq(moves)
-#                 rook_blocked_twice.find_rook_moves("c4", player_moves, opp_moves)
-#             end
-#         end
+            it "returns ['a4', 'b4','d4', 'e4', 'c1', 'c2', 'c3']" do
+                player_moves = ["f4", "d5", "c5"]
+            
+                opp_moves = []
+                moves = ["a4", "b4","d4", "e4", "c1", "c2", "c3"]
+                expect(rook_blocked_twice.find_moves(player_moves, opp_moves)).to eq(moves)
+                rook_blocked_twice.find_moves( player_moves, opp_moves)
+            end
+        end
 
 #         context "when white rook is at c4 with player's own peices obstructing at c5 and at f4 and opponent at c2" do
 #             subject(:rook_blocked_opp) { described_class.new }
