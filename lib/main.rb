d1681267f9f3aa6be10568cb238f1ab86e7d5ac4
jmorton138 <jmorton138@gmail.com>
2021-09-21 08:48:27 +0100

    Add test for Queen class where queen has no obstructing pieces. Refactor Queen.find_moves method so test passes

--- lib/main.rb
+++ lib/main.rb
@@ -518,10 +518,17 @@ class Knight < Piece
 end
 
 class Queen < Piece
+    attr_accessor :position
+    attr_reader :piece_color
 
-    def find_queen_moves(start_pt, player_moves, opp_moves)
-        diagonal_lines = find_bishop_moves(start_pt, player_moves, opp_moves)
-        straight_lines = find_rook_moves(start_pt, player_moves, opp_moves)
+    def initialize(position, piece_color)
+        @position = position
+        @piece_color = piece_color
+    end
+    def find_moves(player_moves, opp_moves)
+        start_pt = self.position
+        diagonal_lines = Bishop.new(start_pt, piece_color).find_moves(player_moves, opp_moves)
+        straight_lines = Rook.new(start_pt, piece_color).find_moves(player_moves, opp_moves)
         moves = diagonal_lines + straight_lines
         moves.sort
     end
